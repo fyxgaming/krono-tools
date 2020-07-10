6948d9892ae781fae6e6126033b03eb7e2a2ce93
David Case <david@kronoverse.io>
2020-07-10 16:33:23 -0400

    Fix cache keys

--- lib/blockchain/rest-blockchain.ts
+++ lib/blockchain/rest-blockchain.ts
@@ -44,31 +44,31 @@ export class RestBlockchain extends Blockchain {
 
     async fetch(txid: string) {
         try {
-            let rawtx = await this.cache.get(`tx:${txid}`);
+            let rawtx = await this.cache.get(`tx://${txid}`);
             if (!rawtx) {
                 const resp = await fetch(`${this.apiUrl}/tx/${txid}`);
                 if(!resp.ok) throw createError(resp.status, resp.statusText);
                 rawtx = await resp.json();
-                await this.cache.set(`tx:${txid}`, rawtx);
+                await this.cache.set(`tx://${txid}`, rawtx);
             }
 
             // console.log(rawtx);
             const tx = new Transaction(Buffer.from(rawtx, 'hex'));
             const locs = tx.outputs.map((o, i) => `${txid}_o${i}`);
             const spends: any[] = await Promise.all(locs.map(async loc => {
-                let spend = await this.cache.get(`spend:${loc}`);
-                if (spend) return spend;
+                let spentTxId = await this.cache.get(`spend://${loc}`);
+                if (spentTxId) return spentTxId;
                 const {txid, index} = loc.split('_o');
-                const resp = await fetch(`${TXQ}/api/v1/txout/txid/${loc}`, { headers: { api_key: API_KEY } });
+                const resp = await fetch(`${TXQ}/api/v1/txout/txid/${txid}/${index}`, { headers: { api_key: API_KEY } });
                 if (!resp.ok) return {};
-                spend = (await resp.json()).result;
-                await this.cache.set(`spend:${loc}`, spend);
-                return spend;
+                spentTxId = (await resp.json()).result.spent_txid;
+                await this.cache.set(`spend://${loc}`, spentTxId);
+                return spentTxId;
             }));
 
             tx.outputs.forEach((o: any, i) => {
-                o.spentTxId = spends[i].spent_txid || null;
-                o.spentIndex = spends[i].spent_index || null;
+                o.spentTxId = spends[i] || null;
+                o.spentIndex = null;
             });
 
             return tx;
