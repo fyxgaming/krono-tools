a780230f7f51c3e7c74000046e4d2a0b41bf814a
David Case <david@kronoverse.io>
2020-07-12 02:09:04 -0400

    add redis-storage

---
+++ lib/storage/redis-storage.ts
@@ -0,0 +1,18 @@
+import { IStorage } from '../interfaces';
+
+export class RedisStorage<T> implements IStorage<T> {
+    constructor(private redis) { }
+
+    async get(key: string) {
+        const state = await this.redis.get(key);
+        return state && JSON.parse(state);
+    }
+
+    async set(key: string, state: T) {
+        await this.redis.set(key, JSON.stringify(state));
+    }
+
+    async delete(key: string) {
+        await this.redis.delete(key);
+    }
+}
