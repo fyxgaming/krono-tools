fdbf90a5dd76bf7c85d6df8ed82c995f214613c0
David Case <david@kronoverse.io>
2020-07-14 02:32:32 -0400

    remove constructor

--- lib/purse.ts
+++ lib/purse.ts
@@ -2,10 +2,6 @@ import { LocalPurse } from '@runonbitcoin/release';
 import { PaymentRequired } from 'http-errors';
 
 export class Purse extends LocalPurse {
-    constructor(options: any) {
-        super(options);
-    }
-
     async pay(rawtx: string) {
         try {
             return super.pay();
