07fc6f1853cbf29c99602b0df04262a4b185f1e9
David Case <david@kronoverse.io>
2020-07-17 01:28:50 -0400

    remove notifiers

--- lib/notifier/index.ts
+++
@@ -1,7 +0,0 @@
-import { EventEmitter } from 'events';
-
-export abstract class Notifier extends EventEmitter {
-    abstract listenAddress(address: string): Promise<void>;
-    abstract listenKinds(kinds: string[]): Promise<void>;
-    abstract listenOrigins(origins: string[]): Promise<void>;
-}
