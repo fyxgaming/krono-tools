1cc77c80aaceec7a2a6f1c7ff9363ce987d8cd5f
David Case <david@kronoverse.io>
2020-07-24 18:27:06 -0400

    fix transaction proxy

--- lib/run-transaction.ts
+++ lib/run-transaction.ts
@@ -4,13 +4,14 @@ export class RunTransaction {
     constructor(run) {
         return new Proxy(run.transaction, {
             get: (target, prop, receiver) => {
+                if(prop === 'actions') return target.actions;
+                if(prop === 'begin') return target.begin();
                 if (prop === 'end') return undefined;
                 if (prop === 'commit') return async () => {
                     let jig = target.actions[0] && target.actions[0].target;
                     if (!jig) return target.rollback();
                     target.end();
                     await jig.sync({ forward: false });
-
                 };
                 if (prop === 'export') return async () => {
                     return target.export().toString();
@@ -19,12 +20,9 @@ export class RunTransaction {
                     const tx = new Transaction(rawtx);
                     return target.import(tx);
                 };
-                return Reflect.get(target, prop, receiver);
-            },
-            // TODO evaluate other traps
-            getOwnPropertyDescriptor: (target, prop) => {
-                if (prop == 'end') return undefined;
-                return Reflect.getOwnPropertyDescriptor(target, prop);
+                if(prop === 'rollback') return target.rollback();
+                if(prop === 'pay') return target.pay();
+                if(prop === 'sign') return target.sign();
             }
         });
     }
