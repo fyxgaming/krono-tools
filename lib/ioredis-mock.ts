fb0f2af46cafadee16e0c39ba3bf34dd5b950ffd
David Case <david@kronoverse.io>
2020-07-24 19:32:23 -0400

    add ioredis-mock

---
+++ lib/ioredis-mock.ts
@@ -0,0 +1,73 @@
+class IORedisMock {
+    private store = new Map<string, any>();
+
+    get(key: string): string {
+        return this.store.get(key);
+    }
+
+    set(key: string, value: string) {
+        return this.store.set(key, value);
+    }
+
+    del(key) {
+        this.store.delete(key);
+    }
+
+    hget(key: string, field: string): string {
+        const item = this.store.get(key);
+        if (!item) return;
+        return item[field];
+    }
+
+    hset(key: string, field: string, value: string) {
+        let item = this.store.get(key);
+        if (!item) item = {};
+        item[field] = value;
+        this.store.set(key, item);
+    }
+
+    hmset(key: string, values: { [key: string]: string }) {
+        const item = Object.assign(this.store.get(key) || {}, values);
+        this.store.set(key, item);
+    }
+
+    hdel(key: string, field: string) {
+        const item = this.store.get(key);
+        delete item[field];
+        this.store.set(key, item);
+    }
+
+    hgetall(key: string): { [key: string]: string } {
+        return this.store.get(key);
+    }
+
+    lindex(key: string, index: number) {
+        const item = this.store.get(key);
+        if (!item) return;
+        return item[index];
+    }
+
+    lrange(key: string, start: number, end: number) {
+        const item = this.store.get(key);
+        if (!item) return;
+        return item.slice(start, end + 1);
+    }
+
+    rpush(key: string, ...items: string[]) {
+        let item = this.store.get(key);
+        if (!item) item = [];
+        item.push(...items);
+    }
+
+    exists(key: string) {
+        return this.store.has(key);
+    }
+
+    expire(key) {
+        return;
+    }
+
+    ttl(key) {
+        return 1000000;
+    }
+}
