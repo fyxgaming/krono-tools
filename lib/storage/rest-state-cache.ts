528b23aa06ec63e8aa17bf93fd880add0e50678e
David Case <david@kronoverse.io>
2020-08-11 12:53:00 -0400

    remove logging

--- lib/storage/rest-state-cache.ts
+++ lib/storage/rest-state-cache.ts
@@ -8,17 +8,13 @@ export class RestStateCache implements IStorage<any> {
     constructor(
         private apiUrl: string, 
         public cache: IStorage<any> = new LRUCache(10000000)
-    ) { 
-        console.log('Cache URL:', apiUrl);
-    }
+    ) { }
 
     async get(key: string): Promise<any> {
         let value = await this.cache.get(key);
         if (value) return value;
-        const url = `${this.apiUrl}/${encodeURIComponent(key)}`;
         try {
-            const resp = await fetch(url);
-            console.log('Status:', resp.status, resp.ok);
+            const resp = await fetch(`${this.apiUrl}/${encodeURIComponent(key)}`);
             if(!resp.ok) {
                 if(resp.status === 404) return;
                 throw createError(resp.status, resp.statusText);
@@ -28,7 +24,7 @@ export class RestStateCache implements IStorage<any> {
                 await this.cache.set(key, value);
             }
         } catch (e) {
-            console.error('State Error', url, e.message, e.stack);
+            console.error('State Error', e.message, e.stack);
             return;
         }
 
