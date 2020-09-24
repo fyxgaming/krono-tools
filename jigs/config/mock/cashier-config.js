bda3378890ff7c969865f2b307e0d55dc5953e00
David Case <david@kronoverse.io>
2020-09-24 12:40:48 -0400

    mint coins on postDeploy

--- jigs/config/mock/cashier-config.js
+++ jigs/config/mock/cashier-config.js
@@ -1,3 +1,4 @@
 class CashierConfig {}
 CashierConfig.pubkey = '038df991cc9d789304e1baf32acc185f9040c03c8b68018c00984690d186b10fc7';
+CashierConfig.address = 'mr21ChbJoVKr9Hk3rbA1RuZe5EehXYVsmv';
 module.exports = CashierConfig;
--- jigs/models/krono-coin.js
+++ jigs/models/krono-coin.js
@@ -1,7 +1,21 @@
-const {Token} = require('@kronoverse/run');
-
-class KronoCoin extends Token {}
+const {Token, Transaction} = require('@kronoverse/run');
+const CashierConfig = require('../config/mock/cashier-config');
+class KronoCoin extends Token {
+    static async postDeploy() {
+        const t = new Transaction();
+        t.update(() => {
+            for(let i = 0; i < 10; i++) {
+                const coin = new KronoCoin(1000000000);
+                coin.send(CashierConfig.address);
+            }
+        });
+        await t.publish();
+    }
+}
 
 KronoCoin.decimals = 4;
+KronoCoin.asyncDeps = {
+    CashierConfig: 'config/{env}/cashier-config.js'
+}
 
 module.exports = KronoCoin;
