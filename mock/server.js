1f5c19adf70860d28a68b6245b3b2bf3448d6d27
David Case <david@kronoverse.io>
2020-10-20 02:15:16 -0400

    remove start

--- mock/server.js
+++ mock/server.js
@@ -321,10 +321,10 @@ blockchain.events.on('txn', async (rawtx) => {
 });
 
 // Testing Stuff
-let PORT = process.env.MOCKPORT === undefined ? 3000 : process.env.MOCKPORT;
+// let PORT = process.env.MOCKPORT === undefined ? 8082 : process.env.MOCKPORT;
 
-(async () => {
-    app.listen(PORT,() => {
-        console.log(`Server listening on port ${PORT}`);
-    })
-})();
+// (async () => {
+//     app.listen(PORT,() => {
+//         console.log(`Server listening on port ${PORT}`);
+//     })
+// })();
