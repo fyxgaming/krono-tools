547d9b1f8d3b425c0988ca5eaf39d84033a23e5e
David Case <david@kronoverse.io>
2020-10-20 22:18:10 -0400

    hide UI

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -3,13 +3,13 @@
 	const walletService = new WalletService();
 	walletService.init();
 
-	export let name: string;
-	import Login from "./components/Login.svelte";
+	// export let name: string;
+	// import Login from "./components/Login.svelte";
 
-	let loggedIn: boolean;
-	const changed = (event: CustomEvent) => {
-		console.log("changed", event.detail);
-	};
+	// let loggedIn: boolean;
+	// const changed = (event: CustomEvent) => {
+	// 	console.log("changed", event.detail);
+	// };
 </script>
 
 <style>
@@ -20,16 +20,16 @@
 		margin: 0 auto;
 	}
 
-	h1 {
+	/* h1 {
 		color: darkslategray;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
-	}
+	} */
 </style>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<!-- <h1>Hello {name}!</h1>
 
-	<Login {loggedIn} on:statusChanged:{changed} />
+	<Login {loggedIn} on:statusChanged:{changed} /> -->
 </main>
