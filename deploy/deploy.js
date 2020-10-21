88f91c982f53888d643f95620b593a3043199a34
David Case <david@kronoverse.io>
2020-10-21 01:05:53 -0400

    fix deployer

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -1,27 +1,31 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = __importDefault(require("dotenv"));
-const fs_extra_1 = __importDefault(require("fs-extra"));
-const minimist_1 = __importDefault(require("minimist"));
-const path_1 = __importDefault(require("path"));
-const rest_blockchain_1 = require("@kronoverse/lib/dist/rest-blockchain");
-const deployer_1 = require("./deployer");
+#!/usr/bin/env node
+
+const dotenv = require('dotenv');
 const fetch = require('node-fetch');
+const fs = require('fs-extra');
+const minimist = require('minimist');
+const path = require('path');
+
+const { RestBlockchain } = require('@kronoverse/lib/dist/rest-blockchain');
+const { Deployer } = require('./deployer');
 const Run = require('@kronoverse/run');
-var argv = minimist_1.default(process.argv.slice(2));
+
+var argv = minimist(process.argv.slice(2));
+
 const blockchainUrls = {
     mock: 'http://localhost:8080',
+    infra: 'https://kronoverse-infra.appspot.com',
+    // dev: 'https://kronoverse-dev.appspot.com',
     adhoc: 'https://adhoc.aws.kronoverse.io',
     dev: 'https://dev.aws.kronoverse.io',
     test: 'https://test.aws.kronoverse.io',
-    prod: 'https://main.aws.kronoverse.io'
+    prod: 'https://prod.aws.kronoverse.io'
 };
+
 console.log('PATH:', process.cwd());
 console.log('ARGV:', argv);
-dotenv_1.default.config({ path: path_1.default.join(process.cwd(), `${argv.env}.env`) });
+dotenv.config({ path: path.join(process.cwd(), `${argv.env}.env`) });
+
 function renderUsage() {
     console.log(`
 
@@ -48,6 +52,7 @@ function renderUsage() {
     `);
     return 'Check usage instructions and provide valid parameters';
 }
+
 (async () => {
     const env = argv.env || 'mock';
     const blockchainUrl = argv.blockchain || process.env.BLOCKCHAIN || blockchainUrls[env];
@@ -57,16 +62,25 @@ function renderUsage() {
     const source = argv.src;
     const catalogFile = argv.catalog || 'catalog.js';
     const disableChainFiles = argv.disableChainFiles;
-    const sourcePath = path_1.default.resolve(source, catalogFile);
+
+    const sourcePath = path.resolve(source, catalogFile);
     console.log(sourcePath);
-    if (!fs_extra_1.default.pathExistsSync(sourcePath))
-        throw new Error(`${source} does not exist`);
+    if (!fs.pathExistsSync(sourcePath)) throw new Error(`${source} does not exist`);
     console.log('CONFIG:', blockchainUrl, network, source);
+    console.log('OWNER:', owner);
+    console.log('PURSE:', purse);
     if (!blockchainUrl || !network || !source) {
         renderUsage();
         process.exit(1);
     }
-    const blockchain = new rest_blockchain_1.RestBlockchain(fetch, blockchainUrl, network, new Run.LocalCache());
+
+    const blockchain = new RestBlockchain(
+        fetch,
+        blockchainUrl, 
+        network, 
+        new Run.LocalCache(),
+    );
+
     const run = new Run({
         blockchain,
         network,
@@ -77,10 +91,13 @@ function renderUsage() {
         trust: '*'
         // logger: console
     });
-    const rootPath = path_1.default.dirname(sourcePath);
+
+    const rootPath = path.dirname(sourcePath)
     console.log('rootPath:', rootPath);
-    const deployer = new deployer_1.Deployer(run, rootPath, env, !disableChainFiles, path_1.default.join(process.cwd(), 'node_modules'));
+    const deployer = new Deployer(run, rootPath, env, !disableChainFiles, path.join(process.cwd(), 'node_modules'));
+
     const catalog = await deployer.deploy(catalogFile);
+
     for (const [agentId, dep] of Object.entries(catalog.agents)) {
         const realm = catalog.realm;
         const resp = await fetch(`${blockchainUrl}/agents/${realm}/${agentId}`, {
@@ -88,12 +105,10 @@ function renderUsage() {
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ location: dep.location })
         });
-        if (!resp.ok)
-            throw new Error(resp.statusText);
+        if(!resp.ok) throw new Error(resp.statusText);
     }
     console.log('Deployed');
 })().catch(e => {
     console.error(e);
     process.exit(1);
 });
-//# sourceMappingURL=deploy.js.map
