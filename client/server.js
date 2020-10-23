c7ff7c7c247cb5b5f212a22ff78ab59ffbb5dec9
David Case <david@kronoverse.io>
2020-10-22 21:05:47 -0400

    add test server

---
+++ client/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const httpProxy = require('express-http-proxy');
+
+
+const app = new express();
+
+app.use('/wallet', httpProxy('http://localhost:5000'));
+app.use(httpProxy('https://adhoc.aws.kronoverse.io'));
+
+app.listen(8000, (err) => {
+    if (err) {
+        console.error('Express Error', err);
+        process.exit(0);
+    }
+    console.log(`App listening on port ${8000}`);
+});
