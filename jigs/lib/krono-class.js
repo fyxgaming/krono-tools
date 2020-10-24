1579bd4005229f661459ad184f5c7aa1a507743a
David Case <david@kronoverse.io>
2020-10-24 01:26:54 -0400

    Fix serialization

--- jigs/lib/krono-class.js
+++ jigs/lib/krono-class.js
@@ -2,26 +2,23 @@ class KronoClass {
     static toObject(skipKeys = [], visited = new Set()) {
         if(visited.has(this)) return;
         visited.add(this);
-        const clone = Object.entries(this).reduce((clone, [key, value]) => {
-            if([...skipKeys, 'deps', 'presets'].includes(key)) return clone;
-            clone[key] = KronoClass.deepClone(value, [], new Set(visited));
-            return clone;
-        }, {})
-        return clone;
+        return this.cloneChildren(this, skipKeys, visited);
     }
     
     static deepClone(obj, skipKeys = [], visited = new Set()) {
         if (!obj || !['object', 'function'].includes(typeof obj)) return obj;
         if(visited.has(obj)) return;
+        if(obj.toObject) return obj.toObject([], new Set(visited));
         visited.add(obj);
-        
-        return obj.toObject ? 
-            obj.toObject([], new Set(visited)) : 
-            Object.entries(obj).reduce((clone, [key, value]) => {
-                if([...skipKeys, 'deps', 'presets'].includes(key)) return clone;
-                clone[key] = this.deepClone(value, [], new Set(visited));
-                return clone;
-            }, Array.isArray(obj) ? [] : {})
+        return this.cloneChildren(obj, skipKeys, visited);   
+    }
+
+    static cloneChildren(obj, skipKeys, visited) {
+        return Object.entries(obj).reduce((clone, [key, value]) => {
+            if([...skipKeys, 'deps', 'presets'].includes(key)) return clone;
+            clone[key] = this.deepClone(value, [], new Set(visited));
+            return clone;
+        }, Array.isArray(obj) ? [] : {})
     }
 }
 KronoClass.sealed = false;
--- jigs/lib/krono-jig.js
+++ jigs/lib/krono-jig.js
@@ -1,30 +1,17 @@
 const { Jig } = require('@kronoverse/run');
+const KronoClass = require('./krono-class');
 
 class KronoJig extends Jig {
     toObject(skipKeys = [], visited = new Set()) {
         if(visited.has(this)) return;
         visited.add(this);
-        const clone = Object.entries(this).reduce((clone, [key, value]) => {
-            if([...skipKeys, 'deps', 'presets'].includes(key)) return clone;
-            clone[key] = KronoClass.deepClone(value, [], new Set(visited));
-            return clone;
-        }, {})
-        return clone;
+        return KronoClass.cloneChildren(this, skipKeys, visited);
     }
 
     static toObject(skipKeys = [], visited = new Set()) {
         if(visited.has(this)) return;
         visited.add(this);
-        const clone = Object.entries(this).reduce((clone, [key, value]) => {
-            if([...skipKeys, 'deps', 'presets'].includes(key)) return clone;
-            clone[key] = KronoClass.deepClone(value, [], new Set(visited));
-            return clone;
-        }, {})
-        return clone;
-    }
-
-    static deepClone(obj, skipKeys, visited = new Set()) {
-        return KronoClass.deepClone(obj, skipKeys, visited);
+        return KronoClass.cloneChildren(this, skipKeys, visited);
     }
 }
 
