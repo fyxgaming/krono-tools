3d1a3379d7378dd5297b4d64ace4c4cfba97814c
David Case <david@kronoverse.io>
2020-12-16 16:27:19 -0500

    Update krono-coin

--- jigs/models/krono-coin.js
+++ jigs/models/krono-coin.js
@@ -2,16 +2,6 @@ const { Token20 } = require('@kronoverse/run').extra;
 
 /* global KronoClass */
 class KronoCoin extends Token20 {
-    // setPayment(payment) {
-    //     this.payment = payment;
-    // }
-
-    // static mint(amount) {
-    //     const coin = super.mint(amount);
-    //     if(caller) coin.setPayment(caller);
-    //     return coin;
-    // }
-    
     static transfer(owner) {
         this.owner = owner;
     }
