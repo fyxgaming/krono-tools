c538ec824ad2c0cecb9d36c6849eb0795d375b6a
David Case <david@kronoverse.io>
2020-12-16 15:27:06 -0500

    Add logging to CashInRequest

--- jigs/agents/test-cashier-agent.js
+++ jigs/agents/test-cashier-agent.js
@@ -3,11 +3,13 @@ const CashierAgent = require('./cashier-agent');
 class TestCashierAgent extends CashierAgent {
     async onCashInRequest(message) {
         const t = this.wallet.createTransaction();
+        let coin;
         t.update(() => {
-            const coin = KronoCoin.mint(25000000);
+            coin = KronoCoin.mint(25000000);
             coin.send(message.payloadObj.owner);
         });
         await t.publish();
+        console.log('Coin:', coin.location);
     }
 }
 
