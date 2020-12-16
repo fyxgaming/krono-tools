de15883f403daa45915e227a5908e2a4262bb3e4
David Case <david@kronoverse.io>
2020-12-16 15:34:48 -0500

    Update owner on cashin

--- client/src/components/WebCashier.svelte
+++ client/src/components/WebCashier.svelte
@@ -126,7 +126,7 @@
                 subject: 'CashInRequest',
                 payload: JSON.stringify({
                     deviceGPS,
-                    owner: ws.wallet.ownerPair.pubKey.toString()
+                    owner: ws.wallet.address
                 }),
             });
 
