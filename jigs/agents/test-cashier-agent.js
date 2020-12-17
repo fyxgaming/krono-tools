2ace64df3a8ac62011ef42385e1d4d188f1829b2
David Case <david@kronoverse.io>
2020-12-16 19:32:20 -0500

    Update test-cashier-agent

--- jigs/agents/test-cashier-agent.js
+++ jigs/agents/test-cashier-agent.js
@@ -1,4 +1,3 @@
-const KronoCoin = require('../models/krono-coin');
 const CashierAgent = require('./cashier-agent');
 
 class TestCashierAgent extends CashierAgent {
@@ -6,12 +5,13 @@ class TestCashierAgent extends CashierAgent {
         const coin = await this.sendCoin(message.payloadObj.owner, 25000000);
         await coin.sync();
         console.log('Coin:', coin.location, coin.amount);
+        return coin.toObject();
     }
 }
 
 TestCashierAgent.asyncDeps = {
     CashierAgent: 'agents/cashier-agent.js',
     KronoCoin: 'models/krono-coin.js',
-}
+};
 
 module.exports = TestCashierAgent;
