51ec4d7c2e0fc2e140e98691246664e64a38b325
Ashish Chandra <ashish.cppdev@gmail.com>
2021-02-08 13:52:35 -0600

    Added postTo to mock cashier config

--- jigs/config/mock/cashier-config.js
+++ jigs/config/mock/cashier-config.js
@@ -1,4 +1,5 @@
 class CashierConfig {}
 CashierConfig.pubkey = '038df991cc9d789304e1baf32acc185f9040c03c8b68018c00984690d186b10fc7';
 CashierConfig.address = 'mr21ChbJoVKr9Hk3rbA1RuZe5EehXYVsmv';
+CashierConfig.postTo = 'http://localhost:8082/cashier';
 module.exports = CashierConfig;
