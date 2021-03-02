9f011ae7245eeef515848c5bd6964aa6bcb0c96c
David Case <david@kronoverse.io>
2021-03-01 19:43:13 -0500

    remove axios from client

--- client/src/services/auth-service.js
+++ client/src/services/auth-service.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
 import * as argon2 from 'argon2-browser';
 import { Bip32, Constants, Ecies, Hash, KeyPair, PrivKey } from 'bsv';
 import { SignedMessage } from '@kronoverse/lib/dist/signed-message';
 import { Buffer } from 'buffer';
+import createError from 'http-errors';
 export class AuthService {
     constructor(apiUrl, network) {
         this.apiUrl = apiUrl;
@@ -34,21 +34,34 @@ export class AuthService {
             recovery: recoveryBuf.toString('base64'),
             email
         };
-        const resp = await axios.post(`${this.apiUrl}/accounts`, new SignedMessage({
-            subject: 'Register',
-            payload: JSON.stringify({
-                id,
-                xpub: bip32.toPublic().toString(),
-                recovery: recoveryBuf.toString('base64'),
-                email
-            })
-        }, keyPair));
+        const resp = await fetch(`${this.apiUrl}/accounts`, {
+            method: 'POST',
+            headers: { 'Content-type': 'application/json' },
+            body: JSON.stringify(new SignedMessage({
+                subject: 'Register',
+                payload: JSON.stringify({
+                    id,
+                    xpub: bip32.toPublic().toString(),
+                    recovery: recoveryBuf.toString('base64'),
+                    email
+                })
+            }, keyPair))
+        });
+        if (!resp.ok)
+            throw createError(resp.status, resp.statusText);
         return keyPair;
     }
     async recover(id, keyPair) {
-        const { data: { path, recovery } } = await axios.post(`${this.apiUrl}/accounts`, new SignedMessage({
-            subject: 'Recover'
-        }), keyPair);
+        const resp = await fetch(`${this.apiUrl}/accounts`, {
+            method: 'POST',
+            headers: { 'Content-type': 'application/json' },
+            body: JSON.stringify(new SignedMessage({
+                subject: 'Recover'
+            }, keyPair))
+        });
+        if (!resp.ok)
+            throw createError(resp.status, resp.statusText);
+        const { path, recovery } = await resp.json();
         const recoveryBuf = Ecies.bitcoreDecrypt(Buffer.from(recovery, 'base64'), keyPair.privKey);
         return {
             xpriv: recoveryBuf.toString(),
@@ -57,12 +70,12 @@ export class AuthService {
     }
     async isIdAvailable(id) {
         try {
-            await axios(`${this.apiUrl}/accounts/${id}`);
-            return false;
+            const resp = await fetch(`${this.apiUrl}/accounts/${id}`);
+            if (!resp.ok && resp.status !== 404)
+                throw createError(resp.status, resp.statusText);
+            return resp.status === 404;
         }
         catch (e) {
-            if (e.status === 404)
-                return true;
             throw e;
         }
     }
