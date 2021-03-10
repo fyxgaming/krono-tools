1b956cce6a398ad2444806ecc34cf2a5a663260f
David Case <david@kronoverse.io>
2021-03-10 16:40:14 -0500

    Update configs

--- jigs/config/dev/cashier-config.js
+++ jigs/config/dev/cashier-config.js
@@ -1,6 +1,6 @@
 class CashierConfig {}
-CashierConfig.pubkey = '02bf87dc0958e8adeffb55659a62ccb3096bc935c7a95f9d5d489bc4c833ac126c';
-CashierConfig.address = 'mvfp7du18nspx6xyAMX5YmvJJMHwV3zr6d';
+CashierConfig.pubkey = '023c4603f443e855d1ec71fcd327d998382fe3dbedbd4026208e2780f9013aca38';
+CashierConfig.address = 'mvfp7du18nspx6xyAMX5YmvJJMHwV3zr6d'; // m/1/0 derivation
 CashierConfig.postTo = 'https://dev.aws.kronoverse.io/cashier';
 CashierConfig.baseUrl = 'https://dev.aws.kronoverse.io';
 CashierConfig.paymentPubkey = '0255e5dba03ff2de55b2b99adcfd8288d84a871702eb6e8ab66232e8d132eacc0c';
--- jigs/config/test/cashier-config.js
+++ jigs/config/test/cashier-config.js
@@ -1,7 +1,10 @@
 class CashierConfig {}
-CashierConfig.pubkey = '038fc7dfaabe72435df983572eda4bd55ca02798e99f88d96729563958c0042231';
+CashierConfig.pubkey = '03ddd9d3ab6cad9253a415914de17310d7d37c8776cb2c487eebac38ace7c636a4';
 CashierConfig.address = 'msWvaBWwpMNq99ibRzBzfdRedfaSXUnhE9';
-CashierConfig.postTo = 'https://test.aws.kronoverse.io/cashier';
 CashierConfig.baseUrl = 'https://test.aws.kronoverse.io';
 CashierConfig.paymentPubkey = '03e876fbff9f42ab1d0629b80d78a45d26608765bf6dab6690de6d7fa379edd7cf';
 module.exports = CashierConfig;
+
+// m/1/0 derivation
+// scripthash = 609594b4f63b173b2510fc6f96629ac8ca20de8036f43281dd9ed4d5f8c8b66d
+// pubkey = 038fc7dfaabe72435df983572eda4bd55ca02798e99f88d96729563958c0042231
