d3852d3dcb9d51f24d7a481eb532159814d1d579
David Case <david@kronoverse.io>
2021-04-07 13:17:33 -0400

    Add ephemeral-catalog

---
+++ jigs/ephemeral-catalog.js
@@ -0,0 +1,30 @@
+class Catalog {
+    static async preDeploy() {
+        this.agents = {
+            cashier: this.deps.CashierAgent,
+            coin: this.deps.KronoCoin,
+        };
+    }
+}
+
+Catalog.realm = 'fyx';
+
+Catalog.asyncDeps = {
+    Agent: 'lib/agent.js',
+    CashierAgent: 'agents/test-cashier-agent.js',
+    CashierConfig: 'config/{env}/cashier-config.js',
+    Dice: 'lib/dice.js',
+    EventEmitter: 'lib/event-emitter.js',
+    JigMap: 'models/jig-map.js',
+    JigSet: 'models/jig-set.js',
+    KronoCoin: 'models/krono-coin.js',
+    KronoClass: 'lib/krono-class.js',
+    KronoError: 'lib/krono-error.js',
+    KronoItem: 'models/krono-item.js',
+    KronoMint: 'models/krono-mint.js',
+    KronoJig: 'lib/krono-jig.js',
+    MockDice: 'lib/mock-dice.js',
+    Sha256: 'lib/sha256.js',
+};
+
+module.exports = Catalog;
