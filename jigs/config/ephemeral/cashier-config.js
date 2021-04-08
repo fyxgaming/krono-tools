1dd2d0e7b50302ca9cb76fa8860a22ed0ef76ecd
David Case <david@kronoverse.io>
2021-04-08 14:22:17 -0400

    Add ephemeral config

---
+++ jigs/config/ephemeral/cashier-config.js
@@ -0,0 +1,5 @@
+class CashierConfig {}
+CashierConfig.pubkey = '03961a409e347c22c5081bc9208bc3732f4f97a1f012b5fb9e9046fc14faf3e9cc';
+CashierConfig.address = 'mfYmPt2dc28i7YDQrA1qo4VMAxQ8VmsGEU';
+CashierConfig.postTo = '/cashier';
+module.exports = CashierConfig;
