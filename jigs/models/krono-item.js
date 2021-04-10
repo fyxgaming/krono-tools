6baa8c915a0a2935571f96eadffe68489015cfd1
David Case <david@kronoverse.io>
2021-04-10 01:49:15 -0400

    Fix defaultSatoshis

--- jigs/models/krono-item.js
+++ jigs/models/krono-item.js
@@ -3,7 +3,7 @@ const KronoJig = require('../lib/krono-jig');
 
 /* global caller */
 class KronoItem extends KronoJig {
-    init(item, owner, metadata = {}, satoshis = Config.satoshis) {
+    init(item, owner, metadata = {}, satoshis = Config.defaultSatoshis) {
         this.item = item;
         this.mint = caller;
         this.minter = caller && caller.owner;
